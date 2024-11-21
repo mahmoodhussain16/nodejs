a55ae38c846a22868c10a85a7ba3fc4b9dc71811
Mahmood Hussain <hussainmahmood952@gmail.com>
2024-11-21 10:28:08 +0500

    express basics & versioning nodejs

--- server/index.js
+++ server/index.js
@@ -1,31 +1,40 @@
 const http=require("http");
-const fs =require("fs");
-const url=require("url");
+// const fs =require("fs");
+// const url=require("url");
+const express=require("express")
 
-const myServer=http.createServer((req,res)=>{
-    const log=`${Date.now()}: ${req.url} New Req Received\n`;
-    const myUrl=url.parse(req.url,true);
-    console.log(myUrl);
-    fs.appendFile("log.txt",log,(err,data)=>{
-        switch(myUrl.pathname){
-            case "/":
-                res.end("HomePage");
-                break;
-            case "/about":
-                const username=myUrl.query.myname;
-                const id=myUrl.query.id;
-                res.end(`Hi,${username}user: ${id}`);
-                break;
-            default:
-                res.end("404 Not Found");
-        }
-    }
-   )
+const app=express();
 
-});
+app.get("/",(req, res)=>{
+    return res.send("hello from home page");
+})
 
-        
-        
+app.get("/about",(req,res)=>{
+    return res.send("About Page"+'Hi'+req.query.name)
+})
+    
 
+app.listen(8000,()=>console.log("server started"));
+// const myServer=http.createServer(app)
+// const myServer=http.createServer((req,res)=>{
+//     const log=`${Date.now()}: ${req.url} New Req Received\n`;
+//     const myUrl=url.parse(req.url,true);
+//     console.log(myUrl);
+//     fs.appendFile("log.txt",log,(err,data)=>{
+//         switch(myUrl.pathname){
+//             case "/":
+//                 res.end("HomePage");
+//                 break;
+//             case "/about":
+//                 const username=myUrl.query.myname;
+//                 const id=myUrl.query.id;
+//                 res.end(`Hi,${username}user: ${id}`);
+//                 break;
+//             default:
+//                 res.end("404 Not Found");
+//         }
+//     }
+//    )
 
-myServer.listen(8000,()=>console.log("server sterted"));
+// });
+
